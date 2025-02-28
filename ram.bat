795ac13494012fa9dcaeb6fce2ba189bf8caafde
IxSfd <jtprdc.ofc@gmail.com>
2025-02-28 11:34:33 -0500

    Create ram.bat

---
+++ ram.bat
@@ -0,0 +1,8 @@
+@echo off
+title RAM Optimization
+color 0A
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v LargeSystemCache /t REG_DWORD /d 1 /f >nul
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 0 /f >nul
+echo [OK] RAM optimized!
+timeout /t 2
+exit
