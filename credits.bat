7bc7875d450328a9251c92b1d0b455666295b80d
IxSfd <jtprdc.ofc@gmail.com>
2025-02-28 11:48:03 -0500

    Create credits.bat

---
+++ credits.bat
@@ -0,0 +1,25 @@
+@echo off
+chcp 65001 >nul
+title X-MENU - CREDITS
+mode con: cols=100 lines=30
+color 0A
+
+:: Enable PowerShell script execution
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process -Force"
+
+cls
+echo ====================================================
+echo  ██   ██ ███    ███ ███████ ██    ██ ██    ██ 
+echo  ██   ██ ████  ████ ██      ██    ██ ██    ██ 
+echo  ███████ ██ ████ ██ █████   ██    ██ ██    ██ 
+echo  ██   ██ ██  ██  ██ ██      ██    ██ ██    ██ 
+echo  ██   ██ ██      ██ ███████  ██████   ██████  
+echo ====================================================
+echo       X-MENU - ULTIMATE SYSTEM OPTIMIZER 
+echo ====================================================
+echo  Developed By      : [ IxSfd ]
+echo  Github            : [ https://github.com/IxSfd/Tool-to-improve-performance ]
+echo ====================================================
+echo.
+echo  Press any key to close X-MENU credits..
+pause>nul
